9affa44df75f4e6a5c11a42ae64735c39793f14b
germanybasillo <160084953+germanybasillo@users.noreply.github.com>
2024-03-10 19:12:59 +0800

    Update web.php

--- project/routes/web.php
+++ project/routes/web.php
@@ -16,36 +16,36 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/a', function () {
-    return view('a');
-});
-
 Route::get('/home', function () {
     return view('home');
 })->name('home');
-
-Route::get('/heroes', function () {
-    return view('heroes');
-})->name('heroes');
-
-Route::get('/news', function () {
-    return view('news');
-})->name('news');
-
-
-Route::get('/contact', function () {
-    return view('contact');
-})->name('contact');
 
 Route::get('/about', function () {
     return view('about');
 })->name('about');
 
-Route::get('/tournaments', function () {
-    return view('tournaments');
-})->name('tournaments');
+Route::get('/event', function () {
+    return view('event');
+})->name('event');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
 
 Route::get('/blog', function () {
     return view('blog');
 })->name('blog');
+
+
+Route::name('joinNow.')->group(function () {
+    Route::get('/bam', function () {
+         return view('bam');
+    })->name('bam');
+     Route::get('/ersvp', function () {
+         return view('ersvp');
+    })->name('ersvp');
+     Route::get('/ml', function () {
+         return view('ml');
+    })->name('ml');
+});
+
