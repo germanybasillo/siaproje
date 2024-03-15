6052f4e8230cbf70a2c5dd1b3695fa891a5cf871
germany <germanybasillo@gmail.com>
2024-03-15 09:44:06 +0800

    first commit

--- project/resources/views/register.blade.php
+++ project/resources/views/register.blade.php
@@ -61,6 +61,10 @@ MEMBER_LOGIN
                 <label for="confirmPassword">Confirm Password</label>
                 <input type="password" class="form-control" id="confirmPassword" placeholder="Confirm Password" required>
             </div>
+            <div class="form-check">
+                <input type="checkbox" class="form-check-input" id="agreeTerms" name="admin">
+                <label class="form-check-label" for="agreeTerms">Check if you are the admin.</label>
+            </div>
             <button type="submit" class="btni">Register</button>
         </form>
     </div>
